9b001f47dad1fdb3f3ad8abd6f63d53f69870b7e
Ruslan Gabbasov <ruslan.gabbasov@gmail.com>
2020-08-29 02:37:34 +0500

    Temp

---
+++ index_files/client_002.js
@@ -0,0 +1,40 @@
+webpackHotUpdate("client",{
+
+/***/ "./src/models/index.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "history", function() { return history; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "models", function() { return models; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "initStore", function() { return initStore; });
+/* harmony import */ var _rematch_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./node_modules/@rematch/core/dist/umd/rematch.js");
+/* harmony import */ var _rematch_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_rematch_core__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var connected_react_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("./node_modules/connected-react-router/esm/index.js");
+/* harmony import */ var history_createHashHistory__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("./node_modules/history/createHashHistory.js");
+/* harmony import */ var history_createHashHistory__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(history_createHashHistory__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _user__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__("./src/models/user.ts");
+
+
+// tslint:disable-next-line:no-var-requires
+
+
+const history = history_createHashHistory__WEBPACK_IMPORTED_MODULE_2___default()();
+const reducers = { router: Object(connected_react_router__WEBPACK_IMPORTED_MODULE_1__["connectRouter"])(history) };
+const models = { user: _user__WEBPACK_IMPORTED_MODULE_3__["user"] };
+const initStore = (models) => Object(_rematch_core__WEBPACK_IMPORTED_MODULE_0__["init"])({
+    redux: {
+        reducers,
+        middlewares: [
+            Object(connected_react_router__WEBPACK_IMPORTED_MODULE_1__["routerMiddleware"])(history)
+        ],
+        devtoolOptions: {}
+    },
+    models
+});
+
+
+/***/ })
+
+})
+//# sourceMappingURL=client.fe31464559353e3c4056.hot-update.js.map
---
+++ index_files/client_003.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("client",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./src/containers/menu/menu.module.sass":
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__("./node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(true);
+// Module
+exports.push([module.i, ".menu-module__menu___3yDTM a {\n  font-size: 22px;\n  color: #2c2738;\n  text-decoration: none;\n  margin: 0 20px;\n  font-weight: 400;\n}\n\n", "",{"version":3,"sources":["D:\\Projects\\Diagnosis\\web\\diagnosis/src\\containers\\menu\\D:\\Projects\\Diagnosis\\web\\diagnosis\\src\\containers\\menu\\menu.module.sass","menu.module.sass"],"names":[],"mappings":"AAEA;EACE,eAAA;EACA,cAAA;EACA,qBAAA;EACA,cAAA;EACA,gBAAA;ACDF","file":"menu.module.sass","sourcesContent":["@import \"../../sass/index\";\n\n.menu a {\n  font-size: 22px;\n  color: $dark-purple;\n  text-decoration: none;\n  margin: 0 20px;\n  font-weight: 400; }\n",".menu a {\n  font-size: 22px;\n  color: #2c2738;\n  text-decoration: none;\n  margin: 0 20px;\n  font-weight: 400;\n}\n\n"]}]);
+// Exports
+exports.locals = {
+	"menu": "menu-module__menu___3yDTM"
+};
+module.exports = exports;
+
+
+/***/ })
+
+})
+//# sourceMappingURL=client.4968af67454b27426954.hot-update.js.map
